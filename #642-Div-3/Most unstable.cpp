ed2f423a372d5e6db25c27e360e35cb52b7b2c80
Aditya Girish Pawate <51478802+AdityaGirishPawate@users.noreply.github.com>
2020-06-06 21:34:09 +0530

    Update Most unstable.cpp

--- #642-Div-3/Most unstable.cpp
+++ #642-Div-3/Most unstable.cpp
@@ -11,22 +11,11 @@ int main()
     while(t--){
         ll n,m;
         cin>>n>>m;
-        cout<<uns(n,m)<<endl;
+        if(n==1) cout<<0<<endl;
+        else if(n==2) cout<<m<<endl;
+        else cout<<2*m<<endl;
     }
 
     return 0;
 }
 
-ll uns(ll n,ll m){
-    ll x,z,temp,rem,ans;
-    if(n==1)
-        return 0;
-    x=n/2;
-    z=n-x;
-    temp=m/x;
-    rem=m%x;
-    if(n%2)
-        return 2*m-temp;
-    else
-        return 2*m;
-}
