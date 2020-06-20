29c27f0386f8546c671f4aafdb84ea2923c0194d
Aditya Girish Pawate <51478802+AdityaGirishPawate@users.noreply.github.com>
2020-06-20 12:54:14 +0530

    Create theatre square.cpp

---
+++ Problem Set/theatre square.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+//const int N=1e5;
+#define INF 1e9
+
+int32_t main() {
+    int n,m,a;
+    cin>>n>>m>>a;
+    cout<<((n-1)/a+1)*((m-1)/a+1)<<endl;
+    return 0;
+}
+
+
