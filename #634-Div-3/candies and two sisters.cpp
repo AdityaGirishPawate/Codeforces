4307720dae0cda2cc6ff3c7582b466ab084b2838
Aditya Girish Pawate <51478802+AdityaGirishPawate@users.noreply.github.com>
2020-06-24 21:11:30 +0530

    Create candies and two sisters.cpp

---
+++ #634-Div-3/candies and two sisters.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+//const int N=1e5;
+#define INF 1e9
+ 
+int32_t main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        int n;
+        cin>>n;
+        cout<<(n-1)/2<<endl;
+    }
+ 
+ 
+    return 0;
+}
