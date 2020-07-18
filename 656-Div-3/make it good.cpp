a0d1c76b487c547b915bdc7da5eff2b385cc177b
Aditya Girish Pawate <51478802+AdityaGirishPawate@users.noreply.github.com>
2020-07-18 15:02:35 +0530

    Create make it good.cpp

---
+++ 656-Div-3/make it good.cpp
@@ -0,0 +1,72 @@
+//const int N=1e5;
+#define INF 1e9
+#include <bits/stdc++.h>
+#include <random>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <climits>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <iomanip>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define f(i,a,b) for(int i=a;i< b;i++)
+#define rep(i,n) f(i,0,n)
+#define fd(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define mp make_pair
+#define vi vector< int >
+#define vl vector< ll >
+#define ss second
+#define ff first
+#define pii pair< int,int >
+#define pll pair< ll,ll >
+#define sz(a) a.size()
+#define all(a) a.begin(),a.end()
+#define tri pair<int,pii>
+#define vii vector<pii>
+#define vll vector<pll>
+#define viii vector<tri>
+#define mod (1e9+7)
+#define pqueue priority_queue< int >
+#define pdqueue priority_queue< int,vi ,greater< int > >
+//#define int long long
+#define int long
+typedef tree<int,null_type,less<>,rb_tree_tag,tree_order_statistics_node_update> o_set;
+vector<vector<int>> g;
+//std::ios::sync_with_stdio(false);
+
+
+int32_t main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        int ptr=n-1;
+        cin>>a[0];
+        f(i,1,n){
+            cin>>a[i];
+        }
+        if(n>2) {
+            while (a[ptr-1]>=a[ptr] && ptr>0)    ptr--;
+            while(a[ptr-1]<=a[ptr] && ptr>0) ptr--;
+            cout<<ptr<<endl;
+        }
+        else cout<<0<<endl;
+    }
+    return 0;
+}
